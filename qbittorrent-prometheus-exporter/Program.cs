01bfb4b1e0dcd6c418ebbad1b99e3e644f7fc675
llanderman <chemdeath69@googlemail.com>
2020-06-29 20:47:34 -0700

    Fixed issue of statistics remaining for deleted torrents

--- qbittorrent-prometheus-exporter/Program.cs
+++ qbittorrent-prometheus-exporter/Program.cs
@@ -41,133 +41,10 @@ namespace qbittorrent_prometheus_exporter
 
     class Program
     {
-        #region Metrics    
-        /// <summary>
-        /// Definitions of all metrics used in this exporter.
-        /// </summary>
-        private static readonly string MetricBytesDownloadedCounterName = "qbittorrent_total_bytes_downloaded";
-        private static readonly Counter MetricBytesDownloaded =
-            Metrics.CreateCounter(MetricBytesDownloadedCounterName
-                , "The total number of bytes downloaded using the Qbittorrent instance"
-                , new CounterConfiguration
-                {
-                    LabelNames = new[] { "torrent", "hash" }
-                });
-
-        private static readonly string MetricBytesUploadedCounterName = "qbittorrent_total_bytes_uploaded";
-        private static readonly Counter MetricBytesUploaded =
-            Metrics.CreateCounter(MetricBytesUploadedCounterName
-                , "The total number of bytes uploaded using the Qbittorrent instance"
-                , new CounterConfiguration
-                {
-                    LabelNames = new[] { "torrent", "hash" }
-                });
-
-        private static readonly string MetricDownloadSpeedGaugeName = "qbittorrent_current_download_speed";
-        private static readonly Gauge MetricDownloadSpeed =
-            Metrics.CreateGauge(MetricDownloadSpeedGaugeName
-                , "The current download speed using the Qbittorrent instance"
-                , new GaugeConfiguration
-                {
-                    LabelNames = new[] { "torrent", "hash" }
-                });
-
-        private static readonly string MetricUploadSpeedGaugeName = "qbittorrent_current_upload_speed";
-        private static readonly Gauge MetricUploadSpeed =
-            Metrics.CreateGauge(MetricUploadSpeedGaugeName
-                , "The current upload speed using the Qbittorrent instance"
-                , new GaugeConfiguration
-                {
-                    LabelNames = new[] { "torrent", "hash" }
-                });
-
-        private static readonly string MetricTotalSizeGaugeName = "qbittorrent_torrent_total_size";
-        private static readonly Gauge MetricTotalSize =
-            Metrics.CreateGauge(MetricTotalSizeGaugeName
-                , "The current total torrent size using the Qbittorrent instance"
-                , new GaugeConfiguration
-                {
-                    LabelNames = new[] { "torrent", "hash" }
-                });
-
-        private static readonly string MetricRemainingSizeGaugeName = "qbittorrent_torrent_remaining_size";
-        private static readonly Gauge MetricRemainingSize =
-            Metrics.CreateGauge(MetricRemainingSizeGaugeName
-                , "The current remaining torrent size using the Qbittorrent instance"
-                , new GaugeConfiguration
-                {
-                    LabelNames = new[] { "torrent", "hash" }
-                });
-
-        private static readonly string MetricAvailableGaugeName = "qbittorrent_torrent_available_amount";
-        private static readonly Gauge MetricAvailable =
-            Metrics.CreateGauge(MetricAvailableGaugeName
-                , "The availability of a torrent with less than 1 being unfulfilled"
-                , new GaugeConfiguration
-                {
-                    LabelNames = new[] { "torrent", "hash" }
-                });
-
-        private static readonly string MetricPeersConnectedGaugeName = "qbittorrent_torrent_peers_connected";
-        private static readonly Gauge MetricPeersConnected =
-            Metrics.CreateGauge(MetricPeersConnectedGaugeName
-                , "The number of peers connected for a specific torrent"
-                , new GaugeConfiguration
-                {
-                    LabelNames = new[] { "torrent", "hash" }
-                });
-
-        private static readonly string MetricPeersInSwarmGaugeName = "qbittorrent_torrent_peers_in_swarm";
-        private static readonly Gauge MetricPeersInSwarm =
-            Metrics.CreateGauge(MetricPeersInSwarmGaugeName
-                , "The number of peers in swarm for a specific torrent"
-                , new GaugeConfiguration
-                {
-                    LabelNames = new[] { "torrent", "hash" }
-                });
-
-        private static readonly string MetricSeedsConnectedGaugeName = "qbittorrent_torrent_seeds_connected";
-        private static readonly Gauge MetricSeedsConnected =
-            Metrics.CreateGauge(MetricSeedsConnectedGaugeName
-                , "The number of seeds connected for a specific torrent"
-                , new GaugeConfiguration
-                {
-                    LabelNames = new[] { "torrent", "hash" }
-                });
-
-        private static readonly string MetricSeedsInSwarmGaugeName = "qbittorrent_torrent_seeds_in_swarm";
-        private static readonly Gauge MetricSeedsInSwarm =
-            Metrics.CreateGauge(MetricSeedsInSwarmGaugeName
-                , "The number of seeds in swarm for a specific torrent"
-                , new GaugeConfiguration
-                {
-                    LabelNames = new[] { "torrent", "hash" }
-                });
-
-        private static readonly string MetricRatioGaugeName = "qbittorrent_torrent_ratio";
-        private static readonly Gauge MetricRatio =
-            Metrics.CreateGauge(MetricRatioGaugeName
-                , "The current ratio for a specific torrent"
-                , new GaugeConfiguration
-                {
-                    LabelNames = new[] { "torrent", "hash" }
-                });
-
-        private static readonly string MetricProgressGaugeName = "qbittorrent_torrent_progress";
-        private static readonly Gauge MetricProgress =
-            Metrics.CreateGauge(MetricProgressGaugeName
-                , "The current progress for a specific torrent"
-                , new GaugeConfiguration
-                {
-                    LabelNames = new[] { "torrent", "hash" }
-                });
-        #endregion
-
         static void Main(string[] args)
         {
-            // Note:  This is a really simple implementation, but it works.
-            Dictionary<string, long> LastBytesDownloadedState = new Dictionary<string, long>();
-            Dictionary<string, long> LastBytesUploadedState = new Dictionary<string, long>();
+            List<string> removeHashes = new List<string>();
+            Dictionary<string, MetricsTorrent> torrentMetrics = new Dictionary<string, MetricsTorrent>();
 
             CLOptions options = null;
             Parser.Default.ParseArguments<CLOptions>(args)
@@ -183,8 +60,6 @@ namespace qbittorrent_prometheus_exporter
             Console.WriteLine($"Creating qbittorrent WebAPI client on {options.QbittorrentIpAddress}:{options.QbittorrentPort}");
             QBittorrentClient client = new QBittorrentClient(new Uri($"http://{options.QbittorrentIpAddress}:{options.QbittorrentPort}"));
 
-            //UTorrentClient client = new UTorrentClient(options.QbittorrentIpAddress, options.QbittorrentPort,
-            //    options.QbittorrentUsername, options.QbittorrentPassword);
             bool reconnectRequired = true;
             for (; ; )
             {
@@ -205,57 +80,37 @@ namespace qbittorrent_prometheus_exporter
 
                     if (response != null)
                     {
+                        foreach (var trackedTorrent in torrentMetrics)
+                        {
+                            trackedTorrent.Value.BeginPoll();
+                        }
+
                         // summarize all sub-torrent data
                         foreach (var torrent in response)
                         {
-                            if (torrent.Downloaded.HasValue)
+                            if (torrentMetrics.ContainsKey(torrent.Hash))
                             {
-                                // MetricBytesDownloaded
-                                if (LastBytesDownloadedState.ContainsKey(torrent.Hash))
-                                {
-                                    var increaseAmt = torrent.Downloaded.Value - LastBytesDownloadedState[torrent.Hash];
-                                    MetricBytesDownloaded.WithLabels(SanitizeString(torrent.Name), torrent.Hash).Inc(increaseAmt);
-                                    if (options.Verbose) Console.WriteLine($"{MetricBytesDownloadedCounterName} with torrent = \"{SanitizeString(torrent.Name)}\" increased by {increaseAmt}.");
-                                }
-
-                                LastBytesDownloadedState[torrent.Hash] = torrent.Downloaded.Value;
+                                torrentMetrics[torrent.Hash].UpdateStatus(torrent);
                             }
-
-                            if (torrent.Uploaded.HasValue)
+                            else
                             {
-                                // MetricBytesUploaded
-                                if (LastBytesUploadedState.ContainsKey(torrent.Hash))
-                                {
-                                    var increaseAmt = torrent.Uploaded.Value - LastBytesUploadedState[torrent.Hash];
-                                    MetricBytesUploaded.WithLabels(SanitizeString(torrent.Name), torrent.Hash).Inc(increaseAmt);
-                                    if (options.Verbose) Console.WriteLine($"{MetricBytesUploadedCounterName} with torrent = \"{SanitizeString(torrent.Name)}\" increased by {increaseAmt}.");
-                                }
-
-                                LastBytesUploadedState[torrent.Hash] = torrent.Uploaded.Value;
-                            }
-
-                            MetricDownloadSpeed.WithLabels(SanitizeString(torrent.Name), torrent.Hash).Set(torrent.DownloadSpeed);
-                            if (options.Verbose) Console.WriteLine($"{MetricDownloadSpeedGaugeName} with torrent = \"{SanitizeString(torrent.Name)}\" set to {torrent.DownloadSpeed}.");
-                            MetricUploadSpeed.WithLabels(SanitizeString(torrent.Name), torrent.Hash).Set(torrent.UploadSpeed);
-                            if (options.Verbose) Console.WriteLine($"{MetricUploadSpeedGaugeName} with torrent = \"{SanitizeString(torrent.Name)}\" set to {torrent.UploadSpeed}.");
-
-                            MetricTotalSize.WithLabels(SanitizeString(torrent.Name), torrent.Hash).Set(torrent.TotalSize??0);
-                            if (options.Verbose) Console.WriteLine($"{MetricTotalSizeGaugeName} with torrent = \"{SanitizeString(torrent.Name)}\" set to {torrent.Size}.");
-                            MetricRemainingSize.WithLabels(SanitizeString(torrent.Name), torrent.Hash).Set(torrent.IncompletedSize??0);
-                            if (options.Verbose) Console.WriteLine($"{MetricRemainingSizeGaugeName} with torrent = \"{SanitizeString(torrent.Name)}\" set to {torrent.IncompletedSize??0}.");
-                            MetricPeersConnected.WithLabels(SanitizeString(torrent.Name), torrent.Hash).Set(torrent.ConnectedLeechers);
-                            if (options.Verbose) Console.WriteLine($"{MetricPeersConnectedGaugeName} with torrent = \"{SanitizeString(torrent.Name)}\" set to {torrent.ConnectedLeechers}.");
-                            MetricPeersInSwarm.WithLabels(SanitizeString(torrent.Name), torrent.Hash).Set(torrent.TotalLeechers);
-                            if (options.Verbose) Console.WriteLine($"{MetricPeersInSwarmGaugeName} with torrent = \"{SanitizeString(torrent.Name)}\" set to {torrent.TotalLeechers}.");
-                            MetricSeedsConnected.WithLabels(SanitizeString(torrent.Name), torrent.Hash).Set(torrent.ConnectedSeeds);
-                            if (options.Verbose) Console.WriteLine($"{MetricSeedsConnectedGaugeName} with torrent = \"{SanitizeString(torrent.Name)}\" set to {torrent.ConnectedSeeds}.");
-                            MetricSeedsInSwarm.WithLabels(SanitizeString(torrent.Name), torrent.Hash).Set(torrent.TotalSeeds);
-                            if (options.Verbose) Console.WriteLine($"{MetricSeedsInSwarmGaugeName} with torrent = \"{SanitizeString(torrent.Name)}\" set to {torrent.TotalSeeds}.");
-                            MetricRatio.WithLabels(SanitizeString(torrent.Name), torrent.Hash).Set(torrent.Ratio);
-                            if (options.Verbose) Console.WriteLine($"{MetricRatioGaugeName} with torrent = \"{SanitizeString(torrent.Name)}\" set to {torrent.Ratio}.");
-                            MetricProgress.WithLabels(SanitizeString(torrent.Name), torrent.Hash).Set(torrent.Progress);
-                            if (options.Verbose) Console.WriteLine($"{MetricProgressGaugeName} with torrent = \"{SanitizeString(torrent.Name)}\" set to {torrent.Progress}.");
+                                var newTorrent = new MetricsTorrent(torrent, options);
+                                torrentMetrics.Add(torrent.Hash, newTorrent);
+                                newTorrent.UpdateStatus(torrent);
+                            }                            
                         }
+                        
+                        foreach (var trackedTorrent in torrentMetrics)
+                        {
+                            trackedTorrent.Value.EndPoll();
+                            if (trackedTorrent.Value.TorrentDeleted)
+                                removeHashes.Add(trackedTorrent.Value.Hash);
+                        }
+                        foreach (var item in removeHashes)
+                        {
+                            torrentMetrics.Remove(item);
+                        }
+                        removeHashes.Clear();
                     }
                 }
                 catch (Exception ex)
@@ -266,11 +121,6 @@ namespace qbittorrent_prometheus_exporter
 
                 System.Threading.Thread.Sleep(TimeSpan.FromSeconds(options.QbitorrentPollSeconds));
             }
-        }
-
-        static string SanitizeString(string input)
-        {
-            return input.Replace("\"", "").Replace("\\", "");
         }
     }
 }
